8a6739a46eb0a731e45cfbb92f53784c07bbdf6d
paulapompeo <paulapompeoc@gmail.com>
2021-10-27 23:11:42 +0200

    Adds input validation

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -13,35 +13,34 @@ import avatar from '../../assets/profile.png'
 
 import * as S from './styles'
 
-export function Dashboard({handleThemeChange, theme}) {
-
-  console.log({theme})
-
+export function Dashboard({ handleThemeChange, theme }) {
   const [user, setUser] = useState('');
   const [userData, setUserData] = useState({});
-  const [inputError, setInputError] = useState('');
-
-  // console.log({ inputError })
-  // console.log({ userData })
-  // console.log(userData.name)
+  const [inputError, setInputError] = useState(false);
 
   async function handleAddRepositories(event) {
     event.preventDefault()
 
     if (!user) {
-      setInputError('Digite o autor/nome do repositório');
-      return;
+      setInputError(true);
     }
+
     try {
       const response = await api.get(`users/${user}`)
       const { data } = response
-
       setUserData(data)
-      setUser(''); //para limpar o input
-      setInputError('');
+      setInputError(false);
     } catch {
-      setInputError('Erro na busca por esse usario'); // todo: change errors
+      setInputError(true);
     }
+  }
+
+  function dateFormat(date) {
+    const [year, month, day] = date.substring(0, 10).split('-')
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const mesCorreto = months[Number(month - 1)]
+
+    return `${day} ${mesCorreto} ${year}`
   }
 
   return (
@@ -52,7 +51,7 @@ export function Dashboard({handleThemeChange, theme}) {
             <h1>devfinder</h1>
             <div className="app-theme">
               <button type="button" onClick={handleThemeChange}>
-                <span>{theme === 'dark' ? 'LIGHT': 'DARK'}</span>
+                <span>{theme === 'dark' ? 'LIGHT' : 'DARK'}</span>
                 <img src={theme === 'dark' ? iconSun : iconMoon} alt="light-theme" />
               </button>
             </div>
@@ -68,12 +67,14 @@ export function Dashboard({handleThemeChange, theme}) {
                 onChange={event => setUser(event.target.value)}
               />
             </div>
-            <button
-              type="submit"
-              // onClick={() => validateEmail(email)}
-            >
-              Search
-            </button>
+            <div>
+              {inputError && <span>No results</span>}
+              <button
+                type="submit"
+              >
+                Search
+              </button>
+            </div>
           </S.Form>
           <S.UserData>
             <img src={userData.avatar_url ? userData.avatar_url : avatar} alt="avatar" />
@@ -81,14 +82,12 @@ export function Dashboard({handleThemeChange, theme}) {
               <div className="user-main-info">
                 <div className="user-name">
                   <h2>{userData.name ? userData.name : 'The Octocat'}</h2>
-                  {/* TODO: deixar dinamico */}
-                  <span>@octocat</span> 
+                  <span>@octocat</span>
                 </div>
-                {/* TODO: fazer função para horário */}
-                <p>{userData.created_at ? `Joined ${userData.created_at}` : 'Joined 25 Jan 2011'}</p> 
+                <p>{userData.created_at ? `Joined ${dateFormat(userData.created_at)}` : 'Joined 25 Jan 2011'}</p>
               </div>
               <div className="user-bio">
-              <p>{userData.bio ? userData.bio : 'This profile has no bio'}</p>
+                <p>{userData.bio ? userData.bio : 'This profile has no bio'}</p>
               </div>
               <S.GithubData>
                 <div className="github-info">
@@ -135,6 +134,3 @@ export function Dashboard({handleThemeChange, theme}) {
     </>
   )
 }
-
-// TODO: o perfil octocat realmente existe, então após submeter as alterações do front para p
-// front-end mentor, alterar para 'Not Available' as infos de social.
